9c7cc3dee6c70a4745c9991d37b21a8c2aa42ee5
josejayesh2020 <97804134+josejayesh2020@users.noreply.github.com>
2023-08-07 21:02:39 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-resource "local_file" "cat" {
-  filename = var.filename[count.index]
-  content  = "Hello super dog"
-  count    = length(var.filename)
-}
-output "name" {
-  value = local_file.cat
-  sensitive = true
-}
-jjose@AQZSXC meta_arguments_count % cat variables.tf
 variable "filename" {
   type = list(string)
   default = [
