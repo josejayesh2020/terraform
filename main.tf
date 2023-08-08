ee10a33f696beb2d671d3aa55f92f136372f637b
josejayesh2020 <97804134+josejayesh2020@users.noreply.github.com>
2023-08-07 20:50:41 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "local_file" "cat" {
+  filename = var.filename[count.index]
+  content  = "Hello super dog"
+  count    = length(var.filename)
+}
+output "name" {
+  value = local_file.cat
+  sensitive = true
+}
